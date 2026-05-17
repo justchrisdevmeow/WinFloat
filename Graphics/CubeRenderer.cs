a4a5b5b196510237300bb067548e797262496d1c
notchwis <kodenothing@gmail.com>
2026-05-17 18:50:27 -0500

    Update CubeRenderer.cs

--- Graphics/CubeRenderer.cs
+++ Graphics/CubeRenderer.cs
@@ -5,6 +5,9 @@ using System.Runtime.InteropServices;
 using Assimp;
 using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL4;
+using Vector3 = OpenTK.Mathematics.Vector3;
+using Vector2 = OpenTK.Mathematics.Vector2;
+using Matrix4 = OpenTK.Mathematics.Matrix4;
 
 namespace WinFloat;
 
